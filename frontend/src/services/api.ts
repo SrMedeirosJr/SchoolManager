1896e75d9f1f1cce8a0e562001f67ae494e402d4
SrMedeirosJr <aleexmedeiros.jr@gmail.com>
2025-03-07 06:08:46 -0300

    Feat: Configuração de CORS para permitir frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://schoolmanager-production-4cea.up.railway.app',
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
+
 
 export default api;
