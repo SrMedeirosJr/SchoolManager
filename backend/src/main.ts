fe64d0d0ed62efafa22abf74075f1d9e59645020
SrMedeirosJr <aleexmedeiros.jr@gmail.com>
2025-03-07 21:18:43 -0300

    feat: Corrigindo problema no CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,19 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as dotenv from 'dotenv';
-
-dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env.development' });
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.CORS_ORIGIN, //"*", //
+    origin: ["https://school-manager-sand.vercel.app", "http://localhost:3001"], 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true,
+    credentials: true, 
   });
 
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(3000);
 }
-
 bootstrap();
