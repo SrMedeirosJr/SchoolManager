5c30b5840b60fb3f330170683474964e027262d2
SrMedeirosJr <aleexmedeiros.jr@gmail.com>
2025-03-07 21:00:53 -0300

    Feat: corrigindo telas de dashborad e cadastro financeiro

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,12 +1,15 @@
 import axios from "axios";
 
-console.log("API Base URL:", process.env.NEXT_PUBLIC_API_URL);
-
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://schoolmanager-production-4cea.up.railway.app",
+  
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'https://schoolmanager-production-4cea.up.railway.app',
   headers: {
     "Content-Type": "application/json",
+
+    
   },
 });
+
+console.log("✅ API Base URL:", process.env.NEXT_PUBLIC_API_URL);
 
 export default api;
